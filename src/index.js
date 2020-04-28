171c7c510f2819607895ca5ae01e01d08afe6b4c
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-28 14:49:11 -0500

    got table with data working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -10,34 +11,21 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-    // let html = '<ul>';
-    // for(let user of users){
-    // }	          }
-    // html += '</ul>';
-    // document.body.innerHTML = html;
-    $(document).ready(function(){
-      console.log('doc is ready');
-      $("#loading").hide();
-    let html = '<table>';
-    for(const movie of movies){
-      html += `<thead><tr><th colspan="3"> Movie info</th></tr></thead>` +
-          `<tbody><tr>
-            <td>NAME</td>
-            <td>ID</td>
-            <td>RATING</td>
-            </tr></tbody>`;
+    console.log('Here are all the movies:');
+    let html = `<tr><th>NAME</th><th>ID</th><th>RATING</th></tr>`;
 
-    }
-    html += `</table>`;
-    $('body').append(html);
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+        $("#loading").hide();
+
+        html += `<tr>
+<td>${title}</td><td>${id}</td><td>${rating}</td>
+</tr>`;
 
 
-
-  })});
+    })
+    $('#table').html(html);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
