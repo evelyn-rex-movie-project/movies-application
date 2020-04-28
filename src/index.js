7405315c4a6b17d8a2cb690f8a92fdde54a42d2b
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-28 15:50:30 -0500

    firgured out how to push to array, but working on getting id to iterate

--- src/index.js
+++ src/index.js
@@ -25,6 +25,20 @@ getMovies().then((movies) => {
 
     })
     $('#table').html(html);
+    $("#add-movie").click(function (e) {
+        e.preventDefault();
+        // let movieID =
+        let movieRating = movies.push($("#movie-rating").val());
+        console.log(movieRating+1);
+        console.log(movies);
+        let movieNameValue = $("#movie-name").val();
+//         let addHTML = `<tr>
+// <td>${movieNameValue}</td>
+// <td>${movieID}</td>
+// <td>${movieRating}</td>
+// </tr>`;
+//         $("#table").append(addHTML);
+    })
 }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
