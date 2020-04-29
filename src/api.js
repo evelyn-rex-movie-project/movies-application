c395c4b70625eba2c51d0806936b39866952c08b
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-29 16:43:51 -0500

    fixed edit put function, need to make edit button function

--- src/api.js
+++ src/api.js
@@ -22,13 +22,26 @@ module.exports = {
         .catch(error => console.error(error));
   },
 
-  updateMovies: (title, rating) => {
-    return fetch('api/movies', {
+  // updateMovies: (title, rating) => {
+  //   return fetch('api/movies', {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(title, rating),
+  //   })
+  //       .then(response => response.json())
+  //       .then(data => console.log(data))
+  //       .catch(error => console.error(error));
+  // },
+
+  editMovies: (data, id) => {
+    return fetch ('api/movies/${id}', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(title, rating),
+      body: JSON.stringify(data),
     })
         .then(response => response.json())
         .then(data => console.log(data))
--- src/index.js
+++ src/index.js
@@ -10,22 +10,30 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 const {postMovie} = require('./api.js');
-const {updateMovies} = require('./api.js');
+const {editMovies} = require('./api.js');
 
 createTable();
+//variables that get our input text into the table
+let movieName = $("#movie-name").val();
+let movieRating = $("#movie-rating").val();
 
+//button to add movies to list
 $("#add-movie").click(function (e) {
     e.preventDefault();
-    let movieName = $("#movie-name").val();
-    let movieRating = $("#movie-rating").val();
     console.log(movieName, movieRating);
     postMovie({"title": movieName,
         "rating": movieRating
     });
     createTable();
-
-
 });
+
+//button to edit existing movies
+$("#edit").click(function(e){
+    e.preventDefault();
+    $("#edit-movie-title").innerText();
+    editMovies();
+})
+
 
 function createTable() {
     getMovies().then((movies) => {
@@ -37,7 +45,11 @@ function createTable() {
             $("#loading").hide();
 
             html += `<tr>
-<td>${title}</td><td>${id}</td><td>${rating}</td>
+<td>${title}</td>
+<td>${id}</td>
+<td>${rating}</td>
+<td><button id="edit">Edit</button>
+</td>
 </tr>`;
 
 
